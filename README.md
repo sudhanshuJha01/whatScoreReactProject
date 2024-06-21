50970c7b509fb809170bf7787ab447bc55ad6f6a
sudhanshuJha01 <sudhanshujha883@gmail.com>
2024-06-21 16:53:35 +0530

    worked on the MatchInfo Router:

--- README.md
+++ README.md
@@ -1,10 +1,4 @@
-# React + Vite
+# WhatScore is the React project
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
-
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
-
+ it is the website which give the detail about cricket matches in which we can watch live match score
 
--- src/pages/MatchInfo/MatchInfo.jsx
+++ src/pages/MatchInfo/MatchInfo.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import { useMatch } from "../../context/matchContext";
 
 function MatchInfo() {
+  const { currMatchInfo } = useMatch();
+
   return (
-    <div>MatchInfo</div>
-  )
+    <>
+      <div className="min-w-96 min-h-[500px] bg-green-600 rounded-md m-7 text-white">
+        <h1>{currMatchInfo?.matchInfo?.seriesName}</h1>
+        <h2>{currMatchInfo?.matchInfo?.matchDesc}</h2>
+        <div className="text-center">
+          <h2>{`${currMatchInfo.matchInfo?.team1?.teamSName} v/s ${currMatchInfo.matchInfo?.team2?.teamSName}`}</h2>
+          <h3>{currMatchInfo.matchInfo?.status}</h3>
+          <h3>{`Match played at ${currMatchInfo.matchInfo?.venueInfo?.ground} in ${currMatchInfo.matchInfo?.venueInfo?.city}`}</h3>
+        </div>
+        <div className="font-bold text-2xl m-2 flex flex-col">
+          <div className="">
+            <h1>{currMatchInfo.matchInfo?.team1.teamName}</h1>
+            <div className="flex items-center text-center" >
+              <span className="m-2">{`Score: ${currMatchInfo.matchScore?.team1Score?.inngs1?.runs} / ${currMatchInfo.matchScore?.team1Score?.inngs1?.wickets}`}</span>
+              <span className="m-2">{`Overs: ${currMatchInfo.matchScore?.team1Score?.inngs1?.overs} / 20`}</span>
+            </div>
+          </div>
+          <div className="">
+            <h1>{currMatchInfo.matchInfo?.team2.teamName}</h1>
+            <div className="flex items-center text-center" >
+              <span className="m-2">{`Score: ${currMatchInfo.matchScore?.team2Score?.inngs1?.runs} / ${currMatchInfo.matchScore?.team2Score?.inngs1?.wickets}`}</span>
+              <span className="m-2">{`Overs: ${currMatchInfo.matchScore?.team2Score?.inngs1?.overs} / 20`}</span>
+            </div>
+          </div>
+          
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default MatchInfo
+export default MatchInfo;
